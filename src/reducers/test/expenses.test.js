2f6253d52c3a59bf1d7caebd7035ecd248f511d9
Lukasz Sobek <lukaszsobek@me.com>
2018-03-27 11:06:37 +0200

    Adds expense reducer tests

---
+++ src/reducers/test/expenses.test.js
@@ -0,0 +1,44 @@
+import { expensesReducer } from "../"
+
+describe("expenseReducer handles",() => {
+
+    it("default action", () => {
+        const state = []
+        const action = {}
+        expect(expensesReducer(state,action)).toEqual([])   
+    })
+
+    it("ADD_EXPENSE", () => {
+        const state = []
+        const expense = 1
+        const action = { type: "ADD_EXPENSE", expense }
+        expect(expensesReducer(state,action)).toEqual([expense])
+    })
+
+    it("REMOVE_EXPENSE", () => {
+        const state = [{id:1},{id:2},{id:3}]
+        const id = 2
+        const action = { type: "REMOVE_EXPENSE", id }
+        expect(expensesReducer(state,action)).toEqual([
+            {"id": 1}, {"id": 3}
+        ])
+    })
+
+    it("EDIT_EXPENSE when no match", () => {
+        const state = [{id:1},{id:2}]
+        const update = {id: 3, update: {test: "yep"}}
+        const action = { type: "EDIT_EXPENSE", update }
+        expect(expensesReducer(state,action)).toEqual([
+            {"id": 1}, {"id": 2}
+        ])
+    })
+
+    it("EDIT_EXPENSE when match", () => {
+        const state = [{id:1},{id:2}]
+        const update = {test: "yep"}
+        const action = { type: "EDIT_EXPENSE",id: 2, update }
+        expect(expensesReducer(state,action)).toEqual([
+            {"id": 1}, {"id": 2, "test": "yep"}
+        ])
+    })
+})
