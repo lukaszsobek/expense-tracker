f338d02895c2edec8bd5f30b6949c00b88dd60ce
Lukasz Sobek <lukaszsobek@me.com>
2018-04-17 22:05:03 +0200

    Moves output files to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = (env) => {
     return {
         entry: "./src/index.js",
         output: {
-            path: path.join(__dirname,"public"),
+            path: path.join(__dirname,"public","dist"),
             filename: "bundle.js"
         },
         module: {
@@ -61,7 +61,8 @@ module.exports = (env) => {
             : "inline-source-map",
         devServer: {
             contentBase: path.join(__dirname,"public"),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: "/dist/"
         }
     }
 }
