1a502687cb009d573bb15d93d66aac9979dc3bae
Lukasz Sobek <lukaszsobek@me.com>
2018-04-18 09:06:58 +0200

    Adds formatting for date and currency

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,23 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import moment from "moment"
+import numeral from "numeral"
 
 export const ExpenseListItem = ({
         id, description, history, amount, createdAt
     }) => {
-    return (
-        <div>
+        const momentString = moment(createdAt).format("MMM Do, YYYY")
+        const amountString = numeral(amount / 100).format("$0,0.00")
+        
+        return (
             <div>
-            <Link to={`/edit/${id}`}>{description}</Link>
-                - {amount}
-                - {createdAt}
+                <div>
+                <Link to={`/edit/${id}`}>{description}</Link>
+                    - {amountString}
+                    - {momentString}
+                </div>
             </div>
-        </div>
-    )
+        )
 }
 
 export default ExpenseListItem
