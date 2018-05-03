e495e6aec8b2094ef000a3e1a45427bf9ffe3715
Lukasz Sobek <lukaszsobek@me.com>
2018-05-03 19:08:08 +0200

    Adds login page styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,18 @@ import { startLogin } from "../actions"
 
 export const LoginPage = ({ startLogin }) => {
     return (
-        <button onClick={startLogin}>
-            Login
-        </button>
+        <div className="login__imageBg">
+            <div className="login__contentBox">
+                <h1 className="login__title">Expense Tracker</h1>
+                <p className="login__slogan">Track your expenses with this app</p>
+                <button
+                    className="login__button"
+                    onClick={startLogin}
+                >
+                    Log in with Google
+                </button>
+            </div>
+        </div>
     )
 }
 
