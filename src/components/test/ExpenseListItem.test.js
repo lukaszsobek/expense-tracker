27fc108617b404b1e203a5d81245a3f71f235727
Lukasz Sobek <lukaszsobek@me.com>
2018-05-15 19:02:16 +0200

    Fixes date warning message

--- src/components/test/ExpenseListItem.test.js
+++ src/components/test/ExpenseListItem.test.js
@@ -5,7 +5,7 @@ import { ExpenseListItem } from "../ExpenseListItem"
 
 test("Renders ExpenseListItem", () => {
         const props = {
-            id: "1", description: "2", history: "3", amount: "4", createdAt: "5"
+            id: "1", description: "2", history: "3", amount: "4", createdAt: 0
         }
         const wrapper = shallow(<ExpenseListItem {...props} />)
         expect(wrapper).toMatchSnapshot()
