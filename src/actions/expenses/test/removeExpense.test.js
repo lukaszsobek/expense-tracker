f89a6d0680bc9641716d18e00e865f739803c4c7
Lukasz Sobek <lukaszsobek@me.com>
2018-05-16 06:22:58 +0200

    Updates tests

--- src/actions/expenses/test/removeExpense.test.js
+++ src/actions/expenses/test/removeExpense.test.js
@@ -2,9 +2,9 @@ import { removeExpenseApp } from "../../"
 
 describe("removeExpense creates", () => {
     it("removeExpenseApp action", () => {
-        const props = {id: 5}
-        expect(removeExpenseApp(props)).toEqual({
-            "id": 5,
+        const id = 5
+        expect(removeExpenseApp(id)).toEqual({
+            id,
             "type": "REMOVE_EXPENSE"
         })
     })
